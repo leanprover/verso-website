2c0431402fc42404481f4b87f281959e79875964
David Thrane Christiansen <david@davidchristiansen.dk>
2026-03-12 13:06:14 +0100

    chore: add web stats to landing page

--- Main.lean
+++ Main.lean
@@ -26,6 +26,12 @@ def theme : Theme := { Theme.default with
             r!"document.addEventListener('DOMContentLoaded', () => { const lightbox = GLightbox(); });"
           </script>
           {{← builtinHeader }}
+        <!-- Privacy-friendly analytics by Plausible -->
+        <script async src="https://plausible.io/js/pa--0OdxwGCKX8nhJ0vma6XG.js"></script>
+        <script>
+          "window.plausible=window.plausible||function(){(plausible.q=plausible.q||[]).push(arguments)},plausible.init=plausible.init||function(i){plausible.o=i||{}};
+          plausible.init()"
+        </script>
         </head>
         <body>
           <header>
